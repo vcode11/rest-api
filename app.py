18840ae1ceaf487e81194312c6d8ac0ebe8fd310
Vishal Mishra <vishalm434@.gamil.com>
2019-11-18 18:55:38 +0530

    setting up endpoints

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+stores = [
+            {
+                'name':'My Wonderful store',
+                'items':[
+                        {
+                            'name':'My item',
+                            'price':15.99,
+                        },
+                    ]
+            },
+        
+        ]
+# POST /store data: {name: }
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item(name):
+    pass
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+app.run(debug=True)
